5a08b34ef464bd81385177c646355dae84c473ee
marielle <marielle.drouin35@gmail.com>
2024-04-06 10:22:45 +0200

    Toolbar

--- app/src/main/java/com/example/pokedexmobile/MainActivity.java
+++ app/src/main/java/com/example/pokedexmobile/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pokedexmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        androidx.appcompat.widget.Toolbar tb = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
     }
 }
